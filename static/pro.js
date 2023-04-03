14de952ccc7c5ca203d03bed12324cc9c3d820c4
miishin <miishing@gmail.com>
2023-04-02 21:52:02 -0400

    professional page with bouncing line art

--- static/pro.js
+++ static/pro.js
@@ -2,6 +2,8 @@
 const FRAME_RATE = 50;
 const COLOR_VELOCITY = 25;
 const VELOCITY = 5;
+const VELOCITY_CHANGE = 1;
+const MAX_VELOCITY = 10;
 
 var r, g, b;
 var x1, y1, x2, y2;
@@ -26,6 +28,7 @@ function setup() {
   
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+  canvas.parent('content');
   
   frameRate(FRAME_RATE);
   
@@ -40,18 +43,28 @@ function setup() {
   dy2 = random(-VELOCITY, VELOCITY);
 }
 
+
 function moveLine(){
   x1 += dx1;
-  if(x1 < 0 || x1 > windowWidth) dx1 *= -1;
+  if(x1 < 0) dx1 = -1 * constrain(dx1 - VELOCITY_CHANGE, -MAX_VELOCITY, 0);
+  if(x1 > windowWidth) dx1 = -1 * constrain(dx1 + VELOCITY_CHANGE, 0, MAX_VELOCITY);
+  //if(x1 < 0 || x1 > windowWidth) dx1 = -1 * (dx1 + random(-VELOCITY, VELOCITY));
   x2 += dx2;
-  if(x2 < 0 || x2 > windowWidth) dx2 *= -1;
+  if(x2 < 0) dx2 = -1 * constrain(dx2 - VELOCITY_CHANGE, -MAX_VELOCITY, 0);
+  if(x2 > windowWidth) dx2 = -1 * constrain(dx2 + VELOCITY_CHANGE, 0, MAX_VELOCITY);
+  //if(x2 < 0 || x2 > windowWidth) dx2 = -1 * (dx2 + random(-VELOCITY, VELOCITY));
   y1 += dy1;
-  if(y1 < 0 || y1 > windowHeight) dy1 *= -1;
+  if(y1 < 0) dy1 = -1 * constrain(dy1 - VELOCITY_CHANGE, -MAX_VELOCITY, 0);
+  if(y1 > windowHeight) dy1 = -1 * constrain(dy1 + VELOCITY_CHANGE, 0, MAX_VELOCITY);
+  //if(y1 < 0 || y1 > windowHeight) dy1 = -1 * (dy1 + random(-VELOCITY, VELOCITY));
   y2 += dy2;
-  if(y2 < 0 || y2 > windowHeight) dy2 *= -1;
+  if(y2 < 0) dy2 = -1 * constrain(dy2 - VELOCITY_CHANGE, -MAX_VELOCITY, 0);
+  if(y2 > windowHeight) dy2 = -1 * constrain(dy2 + VELOCITY_CHANGE, 0, MAX_VELOCITY);
+  //if(y2 < 0 || y2 > windowHeight) dy2 = -1 * (dy2 + random(-VELOCITY, VELOCITY));
 }
 
 function draw() {
+  if(frameCount == 1234) noLoop();
   incrementColor();
   moveLine();
   line(x1, y1, x2, y2);

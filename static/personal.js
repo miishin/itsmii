81312b652f53bd7da080df0b765325fd8dd98f73
miishin <miishing@gmail.com>
2023-04-04 23:36:26 -0400

    personal

---
+++ static/personal.js
@@ -0,0 +1,28 @@
+const VELOCITY = 5;
+var x, y;
+var vx, vy;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.parent('content');
+  frameRate(50);
+  
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+  
+  vx = random(-VELOCITY, VELOCITY);
+  vy = random(-VELOCITY, VELOCITY);
+  
+  background("#F6D309");
+  textSize(50);
+}
+
+function draw() {
+  background("#F6D309");
+  text('mii', x, y);
+  x += vx;
+  y += vy;
+  if(x < 0 || x > windowWidth - 50) vx *= -1;
+  if(y < 35 || y > windowHeight) vy *= -1;
+}
